b967f6a39340f2c40ec93abb335a331ecea74e62
박종재 <66498240+pjj186@users.noreply.github.com>
2024-12-27 12:56:46 +0900

    [Silver IV] Title: 괄호, Time: 96 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const n = Number(input[0]);
-
 let answer = "";
 
 for(let i = 1; i <= n; i++) {
-  const stack = [];
-  for(let j = 0; j < input[i].length; j++) {
-    const item = input[i][j];
-    if(item === ")" && stack.includes("(")) {
-      stack.pop();
-    } else {
-      stack.push(item);
+    const stack = [];
+    let isValid = true;
+    
+    for(let j = 0; j < input[i].length; j++) {
+        const item = input[i][j];
+        
+        if(item === "(") {
+            stack.push(item);
+        } else {
+            if(stack.length === 0) { 
+                isValid = false;
+                break;
+            }
+            stack.pop();
+        }
     }
-  }
-  if(stack.length === 0) {
-    answer += "YES" + "\n";
-  } else {
-    answer += "NO" + "\n";
-  }
+    
+    answer += (isValid && stack.length === 0) ? "YES\n" : "NO\n";
 }
 
-console.log(answer);
+console.log(answer.trim());

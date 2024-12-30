199abf7a322136303a4ce5f95e290f1a507d3d49
박종재 <66498240+pjj186@users.noreply.github.com>
2024-12-30 09:40:52 +0900

    [Bronze IV] Title: $A + B - C$, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [a, b, c] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+console.log(Number(a) + Number(b) - Number(c));
+
+console.log(a + b - c);
